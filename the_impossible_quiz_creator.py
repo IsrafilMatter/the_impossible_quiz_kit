6eec8fb717fec394a9e8e4d53622d6f9e913e254
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-04-10 17:37:11 +0800

    Pseudocode

---
+++ the_impossible_quiz_creator.py
@@ -0,0 +1,39 @@
+# Author: Israfil Palabay
+# The Impossible Quiz Creator
+# April 10, 2025
+
+# Start Program
+# Initialize GUI window and set title, size, background
+# Load start button in the center
+# Display the start button in the center
+
+# If user clicls start:
+#   Play "fart" sound
+#   Hide start button
+#   Show quiz creator interface
+
+# In quiz creator interface:
+#   Ask user for their username
+#   Ask user to type a question
+#   As user to enter 4 choices (a, b, c, d)
+#   ASk user to select the correct answers
+
+# Show buttons:
+#   Save questions
+#   Instructions
+#   ISRAFILMATTER's Github
+
+# If "instructions" clicked:
+#   show how to write and save a question
+
+# If "ISRAFILMATTER's Github" clicked:
+#   Redirect to Github account of "IsrafilMatter" in browser
+
+# If "save question" clicked:
+#   Validate that all fields are filled
+#   Save the questions, choices, and the answer to "quiz_data.txt"
+#   Clear all fields
+#   Ask if user wants to add another question
+#   If not exit program
+
+# End program
