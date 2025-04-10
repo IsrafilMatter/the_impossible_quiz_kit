867b064017d9ebf6db9c320aefc63cdc174e6cb9
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-04-10 17:46:09 +0800

    Pseudocode

---
+++ the_impossible_quiz_program.py
@@ -0,0 +1,42 @@
+# Author: Israfil Palabay
+# The Impossible Quiz
+# April 10, 2025
+
+# Start Program
+# Initialize GUI window and set title, size, background
+# Load start button image
+# Display the start button in the center
+
+# If user clicls start:
+#   Play "fart" sound
+#   Hide start button
+#   Show game interface
+#   Load questions from quiz_data.txt
+#   Show first question
+
+# In game interface:
+# Display number of lives and skips
+# Display question and 4 answer buttons (A, B, C, D)
+# Display skip button
+
+# If answer is selected:
+#   If correct:
+#   Play "ding" sound
+#   Go to the next question
+#   If wrong:
+#   Play "explosion" sound
+#   Decrease life by 1
+#   If lives = 0:
+#   Show "Game over"
+#   Ask to retry or exit
+#   Else: Show "No skips remaining" message
+
+# For some questions: (random)
+#   Start a countdown timer (5 to 10 seconds)
+#   If time runs out: Play "explosion" sound and lose 1 life
+
+# If all questions answered: 
+#   Show "You Win" message
+#   Exit Program
+
+# End Program
